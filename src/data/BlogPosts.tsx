7f2a88b48ee3563da257b1bdf9ab1c545354696c
Alvie <alvaro.santana23@gmail.com>
2026-04-28 23:28:54 -0600

    New blog post

--- src/data/BlogPosts.tsx
+++ src/data/BlogPosts.tsx
@@ -9,9 +9,63 @@ export const blogPosts = [
   {
     id: "2",
     title: "My mental state",
-    content:
-      "I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, I'm feeling pretty good actually, but you know what, ",
+    content: "I'm feeling pretty good actually ",
     category: "Personal",
     date: "2025-03-16",
+  },
+  {
+    id: "3",
+    title: "Geats: Soul laid bare",
+    content: `So I just finished watching Kamen Rider Geats, and there's something to be said about it, and I'm about to say it.
+    
+    Kamen Rider Geats is a show about many things. For the characters, it's about believing in one's self. It's about believing in others, about connecting with them, listening to them and supporting them to see their full potential, which in a way can only be done if you already believe in yourself first. It's about wishing the best for the world, being generous, but not letting your good nature be exploited. It's about acknowledging your anger at the world, and knowing that sometimes, you're aiming it at the wrong place. It's about knowing that a lot of this world can be fake, and weird, and artificial, and seeking truth in it is frustrating. But seeking your truth, your love, valuing the people around you for what they are, instead of who they want you to be.
+    
+    For its world, it's about fandom. It's about inequality. It's about how we CAN all be happy, and if anyone tells you that one's happiness needs to be stolen for others' happiness to exist, then they can go eat rocks. It's about being able to discern between passion and obssession. It's about how you can be born into a corrupt system, and still be able to decide when to act against corruption. It's about how being born with a gift can be good, but not using it is worse than having it forever.
+    
+    For its visuals, it's sleek, to the point, and flashy. It's colorful in a modern, youthful way, retaining playfulness. It's got some AWESOME assets, like there's a chainsaw sword that can be pumped like a shotgun. There's a revolver that unfolds into a rifle. There's a gun transformation driver. So many rider kicks, sparks and explosions. There's a top and bottom variant for each transformation. OH there's a gunsword. A sword with a gun hilt. That shoots conveniently in sword clashes. The gun transformation also has gungloves, and gunboots, bayonetta style. Each choreography and fight is mezmerizing, with actors giving it their all in sweaty, heavy suits of Kamen Rider armor, doing flips, kicks, and even the worm in one infamous scene.
+
+    For its length, it's... Complicated. The first 20 episodes are exceedingly good television, with every episode being an enchanting ride, a mysterious plot unfolding along an even more mysterious protagonist, and both of these things help flesh and develop the main cast, who fill the role of protagonist until we can get closer to the real protagonist. After that, the main plot kind of gets... resolved, and the titular Geats takes a backseat. Then, the rest of the cast get their own character arcs that were set up in the first 20 episodes. And these arcs ARE good! It's just that they're set in situations where they feel like they're not a part of the world, so the tension isn't quite there for those arcs, making them feel unimportant. Same thing with a lot of characters: The main cast is very tight, but certain side characters and SPECIALLY antagonists suffer, with the biggest loser in that being the 'main antagonist' who really only exists so that the plot doesn't end immediately after the first 30 episodes, but doesn't really do much of anything.
+
+    It's also my first Kamen Rider, which means a lot of special things, but it's specially the first time I've seen such merchandising-first showrunning. Every part of this show is specifically crafted to promote the products, the visuals, the music, and even the writing, with certain key moments being required to air at certain times of the year, hard-locking certain character developments to committee mandated times.
+
+    So how com I'm making a huge writeup, if I have so many complaints? If it's just a show to sell toys, if certain parts of it are rushed, if it's so flawed. Then how come it made me start using my site's blog, after never really using it in the first place?
+    
+    And I think that it's because this means something to me. It means that the human soul exists. The soul exists despite how the world will try to squish and squeeze it. The soul can be seen so long as one brings it to the surface, so long as one can expose themselves enough for it to peek out. The soul can be seen in an show specifically made to market toys to kids, because the writers opened their soul and the soul had something to scream about. The stunt actors had something to prove, and my god did they prove it. The actors had something to say, and they said it. Every part of this production is the soul laid bare for its audience, preparing something special, in a low budget, high time pressure environment. 
+    
+    It's not the first show I watch that has a soul. It's not the first time I've seen the human soul. But this is certainly a unique time where it has touched my own. Where something in me wants to explode from inside me, to say something, or even to say nothing, but just exist. 
+    
+    Day jobs can be that squish. It's the soul crusher. We just accept to exist in a world where our soul is put on hold for hours. A fraction of your day, on most days. Thus, a fraction of your life. And, hey, for these guys making this show, this was their day job. Maybe there was a couple of people who felt their own soul was squished for the length of the production.
+    
+    But at the same time, maybe some things need pressure to cook? In the same way a water balloon will shoot farther under pressure, maybe some people need that pressure to create energy.
+    
+    But that same pressure can rip the balloon before it even explodes. It's a matter of balance, one can say. Many times we'll hear stories of successful artists making one good piece, and it making them so much money that they no longer need to squish their soul. And after that, they often don't bear their soul any longer. But others do?
+    
+    In the end, we live in a world obssessed with production, with making things. I suppose part of that is getting to me. A soul so squished that the balloon must have squished under the surface. Do I need to rebuild it? Where does one even start to fill a soul?
+    
+    But that last paragraph isn't fully the truth. I have to stop pretending, as I clearly have had the desire to create, to let my own soul breathe. I've been drawing, creating games, 3d modeling, writing, and in general letting my creative side breathe in the times that I'm not working. And it has been great, specially when being able to share it with friends and loved ones. But I also have to admit that I still hide my art in shame, despite the fact that I work on it. 
+    
+    Yet when I work on art, I giggle. I smile.
+
+    That's what it's all about, isn't it? Why wait until my art can be squished under a capitalism-sized press for me to bare my soul? The art I'm making already makes me giggle and smile. I find it hard to share, due to the inability to achieve a level of skill that I deem required to not get laughed at. Or seen as someone who you feel forced to appreciate, applauded for sharing but not for what you're displaying.
+
+    And yet, isn't that what I'm praising? I'm praising that the soul made it out of the human body, even if imperfect. Am I waiting for a perfect soul to make it out into the world of man? I don't expect it of anyone, so why should I expect it from myself? I guess I only expect for a soul to be seen, and a soul needs to be filled with something. Emotions, hardships, inspiration and love fill a soul.
+    
+    And I guess this is it, this is where the soul is filled. An infantile, toy-focused marketing show, somehow managed to inject my soul with the desire to create. It's kind of baffling to me that this is what made me reach this point. After all, just last week I read Albert Camus's 'The Stranger' and while it definitely made me feel differently about existing (mostly in funny ways. I'll have to write about that, too), it was finishing Kamen Rider Geats that made me get to this point.
+    
+    Instead of a signature, I'll send this blog post off with even more awesome stuff that happens in Kamen Rider Geats. SPOILERS BELOW:
+    
+    Remember that time Neon got Fantasy? It was super sick to see the character arc where her father accepted that he loves her, even though he created a pretend-daughter to fill in for her real dead daughter, which he still grieves? And then he gets it, he gives it to Neon, who gets her driver core back for literally no reason than it being a cool ass scene? And then her rider kick was flourished with spectral swords? and she could phase through walls and projectiles? That shit was cool.
+    
+    Remember Punk Jack's backstory? Dude, that part about his band partner having his memory/dreams erased, meaning that they didn't dream with being in a band anymore. And then Punk Jack finds them, happy with a wife and child, and Punk Jack wonders to himself "is it possible that he's happier doing this than struggling to achieve his dream?" That shit hit me pretty hard. After all, in your 20's you feel so much pressure to both act wild, make a name for yourself, while also, strapping down and building something worthwhile for your future. In my traditional local community, it's very much a "you're going to be starving in the streets if you even THINK about pursuing your artistic dreams" environment out here. And it's a shame! I've seen many a flame flicker out due to this, getting into more boring areas for work and study, and immediately settling down to marry and carry on their parents' companies or something.
+    
+    Remember how Keiwa's whole character was about how kind and generous he was, even gifting and ceding his own boost buckles to others? Eventually this gets exploited for the pleasure of others. And I feel like that's a really cool message and character theme, specially when the takeaway isn't "stop being kind, it's stupid and gullible" but rather "your kindness can and WILL get exploited. Eventually, you need to think for yourself, and fight for what's yours and your loved ones."
+    
+    Remember Michinanga? I didn't like him at first, but there's something about his character motivation that really resonated with me. He does childish and villanous things, but it comes from a source of genuine anguish and concern for someone he truly cared for, and trying to make sure it never happens again. That anger, once again, was exploited by others for their own gain. Providing a more obvious narrative of vengeance being noxious, but contrasting it with kindness also being exploited paints a whole scene that is a lot more nuanced and interesting.
+    
+    Remember Ace? Wow, that character was really really cool up until the point where he just becomes a spectator. In the final episodes, and in the black geats movie, there's a glimpse at early geats and man. He's just not there. Sure, he always wins in the early episodes, and even when he doesn't win, he wins. But again, this is offset by the supporting cast, specifically Keiwa who loses for the sake of losing most of the time. I found this to be compelling in the same way Giorno Giovanna from Jojo's Bizarre Adventure is compelling: A character who doesn't really have a character arc, as they are a complete character, and instead are an engine for other characters to evolve based on their presence and guidance, not direct help.
+    
+    The rest of the characters... I'm not as charitable for. But neither was the show. I really, desperately wanted more of Beroba, but she got done dirty by splitting her antagonist role with Kekera, a character who became infinitely less interesting as soon as he became a co-villain, rather than a scheming spectator.`,
+    category: "Personal",
+    date: "2026-04-28",
   },
 ];
