a2ae9c7a0623a33546732f5f7500653423c904ac
Alvie <alvaro.santana23@gmail.com>
2026-04-28 23:47:34 -0600

    Update metadata for blog posts

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { blogPosts } from "../data/BlogPosts";
+import { Helmet } from "react-helmet-async";
 
 export default function SinglePost() {
   const { id } = useParams(); // Get the post ID from the URL
@@ -10,17 +11,29 @@ export default function SinglePost() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
-      <h1 className="text-3xl font-bold">{post.title}</h1>
-      <p className="mt-4 text-pico-header-text">
-        {post.content.split("\n").map((i, key) => {
-          return (
-            <div className="blog-paragraph" key={key}>
-              {i}
-            </div>
-          );
-        })}
-      </p>
-    </div>
+    <>
+      <Helmet>
+        <title>{post.title} | My Blog</title>
+        <meta name="description" content={post.title} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.content.slice(0, 30)} />
+        <meta
+          property="og:url"
+          content={`https://www.alvie.dev/blog/${post.id}`}
+        />
+      </Helmet>
+      <div className="max-w-7xl mx-auto p-4">
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <p className="mt-4 text-pico-header-text">
+          {post.content.split("\n").map((i, key) => {
+            return (
+              <div className="blog-paragraph" key={key}>
+                {i}
+              </div>
+            );
+          })}
+        </p>
+      </div>
+    </>
   );
 }
