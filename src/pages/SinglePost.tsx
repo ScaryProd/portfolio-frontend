ae169099f40225eae75a87518529b03f00feed85
Alvie <alvaro.santana23@gmail.com>
2026-04-29 01:41:37 -0600

    implemented bbcode

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { blogPosts } from "../data/BlogPosts";
 import { Helmet } from "react-helmet-async";
+import { render } from '@bbob/react'
 
 export default function SinglePost() {
   const { id } = useParams(); // Get the post ID from the URL
@@ -24,15 +25,9 @@ export default function SinglePost() {
       </Helmet>
       <div className="max-w-7xl mx-auto p-4">
         <h1 className="text-3xl font-bold">{post.title}</h1>
-        <p className="mt-4 text-pico-header-text">
-          {post.content.split("\n").map((i, key) => {
-            return (
-              <div className="blog-paragraph" key={key}>
-                {i}
-              </div>
-            );
-          })}
-        </p>
+        <div className="mt-4 text-pico-header-text">
+          {render(post.content)}
+        </div>
       </div>
     </>
   );
